f662c32efdef36a1049fdd6b4a51309c81efd90f
Sylvain <17965220+SylvainDo@users.noreply.github.com>
2022-07-26 23:51:58 +0200

    CMake: rename PROCESS_X variables to TARGET_X, add Linux aarch64 TARGET_ARCH

--- cmake/importWinrtTheming.cmake
+++ cmake/importWinrtTheming.cmake
@@ -2,4 +2,4 @@ include_external_msproject(winrt-theming "${CMAKE_SOURCE_DIR}/winrt-theming/winr
 
 add_library(winrt-theming::lib INTERFACE IMPORTED)
 set_target_properties(winrt-theming::lib PROPERTIES
-	INTERFACE_LINK_LIBRARIES "${CMAKE_SOURCE_DIR}/winrt-theming/${PROCESS_XARCH}/${BUILD_TYPE}/winrt-theming.lib")
+	INTERFACE_LINK_LIBRARIES "${CMAKE_SOURCE_DIR}/winrt-theming/${TARGET_XARCH}/${TARGET_BUILD_TYPE}/winrt-theming.lib")
